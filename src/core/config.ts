b93eede013100085313811e7611fbd51f189f7db
dev_wandry <dima.development@wandry.com.ua>
2025-11-02 19:40:27 +0200

    feat(sdk): Transform request payload

--- src/core/config.ts
+++ src/core/config.ts
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 // dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 export const getEnv = () => {
-  const apiUrl = "http://wandry-analytics.test/api/v1"; //process.env.API_URL;
-  const endpoint = "/registry/install";
+  const apiUrl = "http://wandry-analytics.test/api/v1/"; //process.env.API_URL;
+  const endpoint = "registry/install";
 
   if (!apiUrl) throw new Error("Api url is not provided");
 

f1bd1545ec1ae13e348daf63af5a05e5621fd117
dev_wandry <dima.development@wandry.com.ua>
2025-11-02 19:52:23 +0200

    fix(build): Fixed build stage

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,6 +4,8 @@ export default defineConfig((options: Options) => ({
   entry: ["src/index.ts"],
   splitting: false,
   sourcemap: true,
+  dts: true,
+  format: ["cjs", "esm"],
   clean: true,
   ...options,
 }));

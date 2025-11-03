82c276118f4eb26478ebcd7783f45ec604e9b765
dev_wandry <dima.development@wandry.com.ua>
2025-11-03 19:58:49 +0200

    feat(sdk): Fixed api url

--- src/core/config.ts
+++ src/core/config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 // dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 export const getEnv = () => {
-  const apiUrl = "http://wandry-analytics.test/api/v1/registry/install"; //process.env.API_URL;
+  const apiUrl = "https://analytics.wandry.com.ua/api/v1/registry/install"; //process.env.API_URL;
 
   if (!apiUrl) throw new Error("Api url is not provided");
 

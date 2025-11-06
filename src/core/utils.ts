8496113c77da594505177e2ebb3b7dd8586bdd6f
dev_wandry <dima.development@wandry.com.ua>
2025-11-06 23:19:21 +0200

    fix(sdk): Fixed request filter

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -12,35 +12,15 @@ export const isSDKRequest = (request: EventRequest): boolean =>
 export const isValidRegistryComponent = (request: EventRequest): boolean => {
   const pathname = new URL(request.url).pathname;
 
-  console.log(`[Wandry Analytics]: Validating request for ${request.url}`);
-
   if (isSDKRequest(request)) return false;
-
-  console.log(`[Wandry Analytics]: is sdk request ${isSDKRequest(request)}`);
 
   if (!isGetRequest(request)) return false;
 
-  console.log(`[Wandry Analytics]: is get request ${isGetRequest(request)}`);
-
   if (!isRegistryPath(request)) return false;
-
-  console.log(
-    `[Wandry Analytics]: is registry path ${isRegistryPath(request)}`
-  );
 
   if (!pathname.endsWith(".json")) return false;
 
-  console.log(
-    `[Wandry Analytics]: is ends with .json ${pathname.endsWith(".json")}`
-  );
-
   if (pathname.includes(encodeURIComponent("{name}"))) return false;
-
-  console.log(
-    `[Wandry Analytics]: is included {name} ${pathname.includes(
-      encodeURIComponent("{name}")
-    )}`
-  );
 
   return true;
 };
